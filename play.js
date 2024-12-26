e31e36689b61a826b55dc74e006e38b3613c11f2
avgamecompany <originalavgamecompany@gmail.com>
2024-12-26 18:44:36 +0200

    Add files via upload

---
+++ play.js
@@ -0,0 +1,17 @@
+
+function prime(x) {
+
+var d =document.createElement("div")
+d.innerHTML =x
+document.getElementById("x").appendChild(d)
+}
+
+
+
+    alert("hello")
+    alert("hello")
+    alert("hello")
+    alert("hello")
+    alert("hello")
+
+
